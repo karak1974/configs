ca613878e4d0ca100fe0500dbd2b66b68500c39e
WoLfY <58589088+karak1974@users.noreply.github.com>
2020-10-02 11:57:14 +0200

    Add files via upload
    Qtile config

---
+++ config.py
@@ -0,0 +1,251 @@
+
+from typing import List  # noqa: F401
+
+from libqtile import bar, layout, widget
+from libqtile.config import Click, Drag, Group, Key, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+
+mod = "mod4"
+terminal = guess_terminal()
+
+keys = [
+    
+    Key([mod], "j", lazy.layout.down()),
+    Key([mod], "k", lazy.layout.up()),
+    Key([mod], "h", lazy.layout.left()),
+    Key([mod], "l", lazy.layout.right()),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left()),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right()),
+    Key([mod, "mod1"], "j", lazy.layout.flip_down()),
+    Key([mod, "mod1"], "k", lazy.layout.flip_up()),
+    Key([mod, "mod1"], "h", lazy.layout.flip_left()),
+    Key([mod, "mod1"], "l", lazy.layout.flip_right()),
+    Key([mod, "control"], "j", lazy.layout.grow_down()),
+    Key([mod, "control"], "k", lazy.layout.grow_up()),
+    Key([mod, "control"], "h", lazy.layout.grow_left()),
+    Key([mod, "control"], "l", lazy.layout.grow_right()),
+    Key([mod, "shift"], "n", lazy.layout.normalize()),
+    Key([mod], "Return", lazy.layout.toggle_split()),
+
+    Key([mod], "space", lazy.layout.next(),
+        desc="Switch window focus to other pane(s) of stack"),
+
+    Key([mod, "shift"], "space", lazy.layout.rotate(),
+        desc="Swap panes of split stack"),
+
+    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack"),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod, "control"], "r", lazy.restart(), desc="Restart qtile"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown qtile"),
+    Key([mod], "r", lazy.spawncmd(),
+        desc="Spawn a command using a prompt widget"),
+]
+
+groups = [Group(i) for i in "asdfuiop"]
+
+for i in groups:
+    keys.extend([
+        # mod1 + letter of group = switch to group
+        Key([mod], i.name, lazy.group[i.name].toscreen(),
+            desc="Switch to group {}".format(i.name)),
+
+        # mod1 + shift + letter of group = switch to & move focused window to group
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
+            desc="Switch to & move focused window to group {}".format(i.name)),
+        # Or, use below if you prefer not to switch to that group.
+        # # mod1 + shift + letter of group = move focused window to group
+        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+        #     desc="move focused window to group {}".format(i.name)),
+    ])
+
+layouts = [
+    #layout.Stack(num_stacks=2),
+    # Try more layouts by unleashing below layouts.
+    layout.Bsp(
+        border_focus="#1793d1",
+        border_normal="#333333",
+        margin=7,
+        border_width=3,
+    ),
+    #layout.Colums(),
+    layout.Matrix(
+        border_focus="#1793d1",
+        border_normal="#333333",
+        margin=5,
+        border_width=3,
+    ),
+    layout.Max(),
+    #layout.MonadTall(),
+    #layout.MonadWide(),
+    #layout.RatioTile(),
+    #layout.Tile(),
+    #layout.TreeTab(),
+    #layout.VerticalTile(),
+    #layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font='sans',
+    fontsize=12,
+    padding=3,
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                #widget.CurrentLayout(
+                #    background="#333333",
+                #    foreground="#1793d1",
+                #), 
+                widget.GroupBox(
+                    active="#1793d1",
+                    borderwidht=1,
+                    background="#333333",
+                    foreground="#1793d1",
+                ),
+                widget.Prompt(
+                    background="#333333",
+                    foreground="#1793d1",
+                ),
+                widget.WindowName(
+                    background="#333333",
+                    foreground="#1793d1",
+                ),
+                widget.Chord(
+                    chords_colors={
+                        'launch': ("#ff0000", "#ffffff"),
+                    },
+                    background="#333333",
+                    foreground="#1793d1",
+                    name_transform=lambda name: name.upper(),
+                ),
+                widget.Systray(
+                    background="#333333",
+                    foreground="#1793d1",
+                ),
+                widget.TextBox(
+                    text="|",
+                    fontsize=15,
+                    background="#333333",
+                    foreground="#1793d1",
+                ),
+                #widget.Net(
+                #    format='{down}↓↑{up}',
+                #    background="#333333",
+                #    foreground="#1793d1",
+                #),
+                #widget.TextBox(
+                #    text='|',
+                #    fontsize=10,
+                #    background="#333333",
+                #    foreground="#1793d1",
+                #),
+                widget.CPU(
+                    background="#333333",
+                    foreground="#1793d1",
+                ),
+                widget.TextBox(
+                    text='|',
+                    fontsize=12,
+                    background="#333333",
+                    foreground="#1793d1",
+                ),
+                widget.Memory(
+                    background="#333333",
+                    foreground="#1793d1",
+                ),
+                widget.TextBox(
+                    text='|',
+                    fontsize=12,
+                    background="#333333",
+                    foreground="#1793d1",
+                ),
+                widget.ThermalSensor(
+                    background="#333333",
+                    foreground="#1793d1",
+                ),
+                widget.TextBox(
+                    text='|',
+                    fontsize=12,
+                    background="#333333",
+                    foreground="#1793d1",
+                ),
+                widget.Battery(
+                        format="{char} {percent:2.0%}",
+                    background="#333333",
+                    foreground="#1793d1",
+                ),
+                widget.TextBox(
+                    text="|",
+                    fontsize=12,
+                    background="#333333",
+                    foreground="#1793d1",
+                ),
+                widget.Clock(
+                    format='%Y-%m-%d %a %I:%M %p',
+                    background="#333333",
+                    foreground="#1793d1",
+                ),
+                widget.QuickExit(
+                    background="#333333",
+                    foreground="#1793d1",
+                ),
+            ],
+            24,
+        ),
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
+main = None  # WARNING: this is deprecated and will be removed soon
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(float_rules=[
+    # Run the utility of `xprop` to see the wm class and name of an X client.
+    {'wmclass': 'confirm'},
+    {'wmclass': 'dialog'},
+    {'wmclass': 'download'},
+    {'wmclass': 'error'},
+    {'wmclass': 'file_progress'},
+    {'wmclass': 'notification'},
+    {'wmclass': 'splash'},
+    {'wmclass': 'toolbar'},
+    {'wmclass': 'confirmreset'},  # gitk
+    {'wmclass': 'makebranch'},  # gitk
+    {'wmclass': 'maketag'},  # gitk
+    {'wname': 'branchdialog'},  # gitk
+    {'wname': 'pinentry'},  # GPG key password entry
+    {'wmclass': 'ssh-askpass'},  # ssh-askpass
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
